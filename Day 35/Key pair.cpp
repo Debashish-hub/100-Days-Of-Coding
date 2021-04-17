7c6b55cc879b0d71e038412d98d40ad7eb7f08fe
debashish <debashish1900@gmail.com>
2021-04-17 21:44:26 +0530

    Updated

---
+++ Day 35/Key pair.cpp
@@ -0,0 +1,49 @@
+// Key Pair 
+
+// Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr 
+// whose sum is exactly X.
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_set<int> map;
+	    for(int i=0; i<n; i++){
+	        if(map.find(x - arr[i]) != map.end()){
+	            return true;
+	        }
+	        map.insert(arr[i]);
+	    }
+	    return false;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
