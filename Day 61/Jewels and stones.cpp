134fb8f749ad61d42735eaef0035441254f5358e
Debashish kumar sahoo <debashish1900@gmail.com>
2021-05-13 23:10:27 +0530

    Day 61

---
+++ Day 61/Jewels and stones.cpp
@@ -0,0 +1,23 @@
+// Jewels and Stones
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. 
+// Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int c = 0;
+		unordered_map<char, int> m;
+		for(int i = 0; i < jewels.length(); i++){
+			m[jewels[i]]++;
+		}
+		for(int j = 0; j < stones.length(); j++){
+			if(m.count(stones[j])){
+				c++;
+			}
+		}
+		return c;   
+    }
+};
---
+++ Day 61/insertion sort.cpp
@@ -0,0 +1,62 @@
+// Insertion Sort 
+
+// The task is to complete the insert() function which is used to implement Insertion Sort.
+
+
+// { Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+class Solution
+{
+    
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+  // } Driver Code Ends
