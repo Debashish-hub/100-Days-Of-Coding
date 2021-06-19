ba4ed64fcdfca98d3566960ec6141e570d658883
Debashish kumar sahoo <debashish1900@gmail.com>
2021-06-19 23:13:50 +0530

    Day 98

---
+++ Day 98/Ceil in BST.cpp
@@ -0,0 +1,145 @@
+// Ceil in BST 
+
+// Given a BST and a number X, find Ceil of X.
+// Note: Ceil(X) is a number that is either equal to X or is immediately greater than X.
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+int findCeil(Node* root, int input);
+
+
+
+int main()
+{
+
+	int t;
+	scanf("%d ", &t);
+	while (t--)
+	{
+		string s;
+		getline(cin >> ws, s);
+		int x;
+		cin >> x;
+		Node* root = buildTree(s);
+		cout << findCeil(root, x) << endl;
+	
+	}
+	return 1;
+}
+
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+
+//Function to return the ceil of given number in BST.
+int findCeil(Node* root, int input) 
+{ 
+    if (root == NULL) 
+        return -1; 
+  
+    // Your code here
+    Node *res = NULL;
+    while(root){
+        if(root->data == input){
+            return root->data;
+        }else if(root->data < input){
+            root = root->right;
+        }else{
+            res = root;
+            root = root->left;
+        }
+    }
+    if(res){
+        return res->data;
+    }else{
+        return -1;
+    }
+} 
---
+++ Day 98/print BST in a given range.cpp
@@ -0,0 +1,132 @@
+// Print BST elements in given range 
+
+// Given a Binary Search Tree and a range. Find all the numbers in the BST that lie in the given range.
+// Note: Element greater than or equal to root go to the right side.
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+vector<int> printNearNodes(Node*root,int k1 , int k2);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        int k1 , k2;
+        string s;
+		getline(cin,s);
+		scanf("%d ", &k1);
+		scanf("%d " , &k2);
+        Node* root = buildTree(s);
+        vector <int> res = printNearNodes(root, k1 ,  k2);
+        for (int i=0;i<res.size();i++) cout << res[i] << " ";
+        cout<<endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+void solve(Node *root, int low, int high, vector<int>&ans){
+      if(root == NULL){
+        return ;
+      }
+      if(root->left){
+          solve(root->left, low, high, ans);
+      }
+      if(root->data >= low && root->data <= high){
+          ans.push_back(root->data);
+      }
+      if(root->right){
+          solve(root->right, low, high, ans);
+      }
+}
+
+
+
+//Function to return a list of BST elements in a given range.
+vector<int> printNearNodes(Node *root, int low, int high)
+{
+  // your code goes here   
+  vector<int> ans;
+  solve(root,low,high,ans);
+  return ans;
+}
